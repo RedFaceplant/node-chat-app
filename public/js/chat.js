32cb32eba3e7433cc0d55e8a38706f37cbba3c4f
RedFaceplant <ashtonking321@gmail.com>
2022-01-18 15:49:26 -0500

    init commit

---
+++ public/js/chat.js
@@ -0,0 +1,125 @@
+const socket = io()
+
+//Elements
+const $messageForm = document.querySelector("#msgForm")
+const $messageInput = $messageForm.querySelector("input")
+const $messageButton = $messageForm.querySelector("button")
+const $URLButton = document.querySelector("#sendURL")
+const $messages = document.querySelector("#messages")
+
+//Templates
+const msgTemplate = document.querySelector("#msgTemplate").innerHTML
+const urlTemplate = document.querySelector("#urlTemplate").innerHTML
+const sidebarTemplate = document.querySelector("#sidebarTemplate").innerHTML
+
+//Options
+const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
+const autoscroll = () => {
+    //New element
+    const $newMessage = $messages.lastElementChild
+    
+    //Get height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+
+    //height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    //scroll distance
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if(containerHeight - newMessageHeight <= scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+
+
+}
+
+socket.on("message", (message) => {
+    //console.log(message)
+    const html = Mustache.render(msgTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format("h:mm A")
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+    autoscroll()
+})
+
+socket.on("URL", (message) => {
+    //console.log(message)
+    const html = Mustache.render(urlTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format("h:mm A")
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+    autoscroll()
+})
+
+socket.on("roomData", ({room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector("#sidebar").innerHTML = html
+})
+
+$messageForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    $messageButton.setAttribute("disabled", "disabled")
+
+    const msg = $messageInput.value
+
+    socket.emit("sendMessage", msg, (error) => {
+        $messageButton.removeAttribute("disabled")
+        $messageInput.value = ""
+        $messageInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+        //console.log("Message sent")
+    })
+})
+
+$URLButton.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    $URLButton.setAttribute("disabled", "disabled")
+
+    const msg = $messageInput.value
+
+    socket.emit("sendURL", msg, (error) => {
+        $URLButton.removeAttribute("disabled")
+        $messageInput.value = ""
+        $messageInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+        //console.log("URL sent")
+    })
+})
+
+socket.emit("join", {username, room}, (error) => {
+    if(error){
+        alert(error)
+        location.href = "/"
+    }
+})
+
+// socket.on("countUpdated", (count) => {
+//     console.log("The Count has been updated!", count)
+// })
+
+// document.querySelector("#increment").addEventListener("click", () => {
+//     console.log("Clicked")
+//     socket.emit("increment")
+// })
---
+++ src/index.js
@@ -0,0 +1,76 @@
+const path = require("path")
+const http = require("http")
+const express = require("express")
+const socketio = require("socket.io")
+const {generateMessage, generateURL} = require("./utils/messages")
+const {addUser, removeUser, getUser, getUsersInRoom} = require("./utils/users")
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, "../public")
+
+app.use(express.static(publicDirectoryPath))
+
+//let count = 0
+
+io.on("connection", (socket) => {
+    //console.log("New Websocket Connection")
+
+    socket.on("join", ({username, room}, callback) => {
+        const {error, user} = addUser({id: socket.id, username, room})
+
+        if(error){
+            return callback(error)
+        }
+
+        socket.join(user.room)
+        
+        socket.emit("message", generateMessage("Server", `Welcome to '${user.room}'!`))
+        socket.broadcast.to(user.room).emit("message", generateMessage("Server", `${user.username} just joined!`))
+        io.to(user.room).emit("roomData", {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
+
+    socket.on("sendMessage", (msg, callback) => {
+        const user = getUser(socket.id)
+
+        io.to(user.room).emit("message", generateMessage(user.username, msg))
+        callback()
+    })
+
+    socket.on("sendURL", (url, callback) => {
+        const user = getUser(socket.id)
+
+        io.to(user.room).emit("URL", generateURL(user.username, url))
+        callback()
+    })
+
+    socket.on("disconnect", () => {
+        const user = removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit("message", generateMessage("Server", `${user.username} has left the chat.`))
+            io.to(user.room).emit("roomData", {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Sever is up on port ${port}!`)
+})
+
+// socket.emit("countUpdated", count)
+// socket.on("increment", () => {
+//     count++
+//     io.emit("countUpdated", count)
+// })
